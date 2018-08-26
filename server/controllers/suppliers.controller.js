ecfd11ea7b67c39b38a9c088c2c55c492cc69623
Pedro Ruiz Hidalgo <correo@pedroruizhidalgo.es>
2018-08-26 13:16:42 +0200

    suppliers controller and routes

---
+++ server/controllers/suppliers.controller.js
@@ -0,0 +1,27 @@
+/**
+* Name: suppliers.controller
+*
+* @author Pedro Ruiz Hidalgo
+*		  correo@pedroruizhidalgo.es
+*         @pedroruizhidalg
+*
+*		  Coding the world since 1983!
+*
+* Location: server/controllers/suppliers.controller
+*
+* Created:  26 Aug 2018
+*
+*
+* Description:
+*
+* Requirements:
+*
+*/
+
+
+
+
+
+/** this ends this file
+* server/controllers/suppliers.controller
+**/
---
+++ server/routes/suppliers.routes.js
@@ -0,0 +1,27 @@
+/**
+* Name: suppliers.routes
+*
+* @author Pedro Ruiz Hidalgo
+*		  correo@pedroruizhidalgo.es
+*         @pedroruizhidalg
+*
+*		  Coding the world since 1983!
+*
+* Location: server/routes/suppliers.routes
+*
+* Created:  26 Aug 2018
+*
+*
+* Description:
+*
+* Requirements:
+*
+*/
+
+
+
+
+
+/** this ends this file
+* server/routes/suppliers.routes
+**/
