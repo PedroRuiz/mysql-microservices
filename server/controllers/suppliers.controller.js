ea2b6d0f9297c68565517c8b3cddf3652d67deae
Pedro Ruiz Hidalgo <correo@pedroruizhidalgo.es>
2018-08-26 14:16:37 +0200

    suppliers_suppliers

--- server/controllers/suppliers.controller.js
+++ server/controllers/suppliers.controller.js
@@ -12,16 +12,83 @@
 * Created:  26 Aug 2018
 *
 *
-* Description:
+* Description: controller for suppliers
 *
-* Requirements:
+* Requirements: '../database.js'
 *
 */
+const { connPromise } = require('../database');
 
+const Ok = {
+    status: "200",
+    response: "Ok"
+};
 
+const TABLE = "suppliers_suppliers";
 
+const suppliersController = {};
 
+suppliersController.getSuppliers = async (req, res) => {
+    
+  await connPromise
+    .then((conn) => conn.query(`SELECT * FROM ${TABLE}`) )
+    .then(([rows, f, err]) => {
+        res.json(!err ? rows : err);
+    });
+};
 
+suppliersController.getSupplier = async (req, res) => {
+    await connPromise
+        .then((conn) => conn.query(`SELECT * FROM ${TABLE} WHERE id=${req.params.id}`))
+        .then(([rows, f, err]) => {
+            res.json(!err ? rows[0] : err);
+        });
+};
+
+suppliersController.deleteSupplier = async (req,res) => {
+    await connPromise
+        .then( (conn) => conn.query(`DELETE FROM ${TABLE} WHERE id = ${req.params.id}`) )
+        .then( ([row, fields, err]) => 
+            res.json( !err ? Ok : err)
+        );
+};
+
+suppliersController.createSupplier = async (req, res) => {
+    await connPromise
+        .then ( (conn) => 
+           conn.execute(
+                `INSERT INTO ${TABLE} (type, tax_id, first_name, last_name) values (?,?,?,?)`,
+                [
+                    req.body.type,
+                    req.body.tax_id,
+                    req.body.first_name,
+                    req.body.last_name
+                ]
+            )
+        )
+        .then( ([rows, fields, error]) => res.json( !error ? Ok : err))
+        .catch ( e => res.json(e))
+};
+
+suppliersController.putSupplier = async (req, res) => {
+    await connPromise
+        .then(conn =>
+            conn.execute(
+                `UPDATE ${TABLE} SET type=?, tax_id=?, first_name=?, last_name=? WHERE id=${req.params.id}`,
+                [
+                req.body.type,
+                req.body.tax_id,
+                req.body.first_name,
+                req.body.last_name
+                ]
+            )
+        )
+        .then( ([rows, fields, error]) => res.json(!error ? Ok : error))
+        .catch( e => res.json(e)) 
+
+};
+
+module.exports = suppliersController;
 /** this ends this file
 * server/controllers/suppliers.controller
 **/
--- server/routes/suppliers.routes.js
+++ server/routes/suppliers.routes.js
@@ -12,16 +12,28 @@
 * Created:  26 Aug 2018
 *
 *
-* Description:
+* Description: routes for suppliers
 *
-* Requirements:
+* Requirements: express, '../controllers/suppliers.controller.js'
 *
 */
 
+const express = require("express");
+const router = express.Router();
+
+const controller = require('../controllers/suppliers.controller');
+
+router.get("/s/", controller.getSuppliers);
+router.get("/s/:id", controller.getSupplier);
+
+router.post('/s', controller.createSupplier);
+
+router.delete('/s/:id', controller.deleteSupplier);
+
+router.put('/s/:id', controller.putSupplier);
 
 
-
-
+module.exports = router;
 /** this ends this file
 * server/routes/suppliers.routes
 **/
